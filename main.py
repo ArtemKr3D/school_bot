0eb35a1969dc38e8d5b3cca1fd723525107a45da
Artem <art.kravtsov10@gmail.com>
2023-06-11 19:42:29 +0300

    PB-02 add "help" method

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ bot = telebot.TeleBot('5596628205:AAH1lUAe11kJ4CfVHcg7d35qtxxXIf8O6a8')
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'Зроблю незвичайну Заставку на meet та google. 1 грн одна заставка. /help ❓ ')
+    bot.send_message(message.chat.id, 'test start command')
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, 'test help command')
 
 bot.polling(none_stop=True)
