da95b5da4dc5b4f6e9318381c0f6577423a0b110
Artem <art.kravtsov10@gmail.com>
2023-06-18 16:43:18 +0300

    PB-11 add buttons and images

--- main.py
+++ main.py
@@ -8,60 +8,86 @@ bot = telebot.TeleBot('6200157896:AAEewNsZ5Vv-7gq39uEf2oIwcyqMCPybB3k')  # test
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Доброго дня! 😁.')
-    bot.send_message(message.chat.id, 'Ви знаєте хто я? /No /Yes')
 
-
-@bot.message_handler(commands=['No'])
-def no(message):
-    bot.send_message(message.chat.id, 'Тоді я розповім 😊.')
-    bot.send_message(message.chat.id,
-                     'Я телеграм бот за допомогою якого ви можете замовити ручку та якісь констовари які я можу вам надати 😎.')
-    bot.send_message(message.chat.id,
-                     'Тепер ви знаєте хто я 🤗. Для детальнішої інформаці ви можете зателефонувати оператору 😉. /help')
-
-
-@bot.message_handler(commands=['Yes'])  # TODO додати команду спонсор
-def yes(message):
-    bot.send_message(message.chat.id, 'Тоді Давайте розпочинати роботу 😁.')
-    bot.send_message(message.chat.id, 'команди чата:')
-    bot.send_message(message.chat.id, '/start - розпочати 🏁')
-    bot.send_message(message.chat.id, '/help - команди чата ❓')
-    bot.send_message(message.chat.id, '/buy - купити 💵')
-
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, 'команди чата:')
-    bot.send_message(message.chat.id, '/start - розпочати 🏁')
-    bot.send_message(message.chat.id, '/help - команди чата ❓')
-    bot.send_message(message.chat.id, '/buy - купити 💵')
-    bot.send_message(message.chat.id, '/to_sponsor - стати моїм спонсором 💸')
-
-
-@bot.message_handler(commands=['to_sponsor'])
-def toSponsor(message):
-    bot.send_message(message.chat.id, 'Дякую що ви хочете стати моїм спонсором 🥰')
-
-
-@bot.message_handler(commands=['buy'])
-def buy(message):
-    button1 = InlineKeyboardButton(text="ручка-стирачка", callback_data="test1")
-    button2 = InlineKeyboardButton(text='стержні до ручки стирачки', callback_data="test2")
+    button1 = InlineKeyboardButton(text="так", callback_data="так")
+    button2 = InlineKeyboardButton(text='ні', callback_data="ні")
 
     markup = InlineKeyboardMarkup()
     markup.add(button1, button2)
 
-    bot.send_message(message.from_user.id, "виберіть товар:", reply_markup=markup)
+    bot.send_message(message.from_user.id, "Ви знаєте хто я?", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "test1")
+@bot.callback_query_handler(func=lambda call: call.data == "так")
 def test1(call):
-    bot.send_message(call.message.chat.id, 'test11')
+    button1 = InlineKeyboardButton(text="команди чата", callback_data="команди чата")
+
+    markup = InlineKeyboardMarkup()
+    markup.add(button1)
+
+    bot.send_message(call.from_user.id, "Тоді Давайте розпочинати роботу 😁.", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "test2")
+@bot.callback_query_handler(func=lambda call: call.data == "ні")
 def test2(call):
-    bot.send_message(call.message.chat.id, 'test22')
+    bot.send_message(call.message.chat.id, 'Тоді я розповім 😊.')
+    bot.send_message(call.message.chat.id,
+                     'Я телеграм бот за допомогою якого ви можете замовити ручку та якісь констовари які я можу вам надати 😎.')
+    bot.send_message(call.message.chat.id,
+                     'Тепер ви знаєте хто я 🤗. Для детальнішої інформаці ви можете зателефонувати оператору 😉.')
+
+    button1 = InlineKeyboardButton(text="команди чата", callback_data="команди чата")
+
+    markup = InlineKeyboardMarkup()
+    markup.add(button1)
+
+    bot.send_message(call.from_user.id, "Давайте розпочинати роботу 😁.", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "розпочати")
+def test1(call):
+    button1 = InlineKeyboardButton(text="так", callback_data="так")
+    button2 = InlineKeyboardButton(text='ні', callback_data="ні")
+
+    markup = InlineKeyboardMarkup()
+    markup.add(button1, button2)
+
+    bot.send_message(call.from_user.id, 'Доброго дня! 😁.')
+    bot.send_message(call.from_user.id, "Ви знаєте хто я?", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "команди чата")
+def test1(call):
+    button1 = InlineKeyboardButton(text="розпочати", callback_data="розпочати")
+    button2 = InlineKeyboardButton(text="команди чата", callback_data="команди чата")
+    button3 = InlineKeyboardButton(text="купити", callback_data="купити")
+    markup = InlineKeyboardMarkup()
+    markup.add(button1, button2, button3)
+
+    bot.send_message(call.from_user.id, "команди чата", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "купити")
+def test1(call):
+    button1 = InlineKeyboardButton(text="ручка-стирачка", callback_data="ручка-стирачка")
+    button2 = InlineKeyboardButton(text='стержні до ручки стирачки', callback_data="стержні до ручки стирачки")
+
+    markup = InlineKeyboardMarkup()
+    markup.add(button1, button2)
+
+    bot.send_message(call.from_user.id, "виберіть товар:", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "ручка-стирачка")
+def test1(call):
+    bot.send_photo(call.message.chat.id, photo=open('images/pen1.jpg', 'rb'))
+    bot.send_message(call.message.chat.id, 'немає в наявності')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "стержні до ручки стирачки")
+def test2(call):
+    bot.send_photo(call.message.chat.id, photo=open('images/15075389.jpg', 'rb'))
+    bot.send_message(call.message.chat.id, 'немає в наявності')
 
 
 bot.polling(none_stop=True)
