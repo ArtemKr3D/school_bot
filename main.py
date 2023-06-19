cd51bf879683fb9c2dba45885328b8fd37c5410e
Artem <art.kravtsov10@gmail.com>
2023-06-19 14:30:49 +0300

    PB-13 add images to buy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = telebot.TeleBot('5596628205:AAH1lUAe11kJ4CfVHcg7d35qtxxXIf8O6a8')#prod
-# bot = telebot.TeleBot('6200157896:AAEewNsZ5Vv-7gq39uEf2oIwcyqMCPybB3k')  # test
+# bot = telebot.TeleBot('5596628205:AAH1lUAe11kJ4CfVHcg7d35qtxxXIf8O6a8')#prod
+bot = telebot.TeleBot('6200157896:AAEewNsZ5Vv-7gq39uEf2oIwcyqMCPybB3k')  # test
 
 buttonYes = InlineKeyboardButton(text="так", callback_data="так")
 buttonNo = InlineKeyboardButton(text='ні', callback_data="ні")
@@ -71,23 +71,36 @@ def test1(call):
 def test1(call):
     button1 = InlineKeyboardButton(text="ручка-стирачка", callback_data="ручка-стирачка")
     button2 = InlineKeyboardButton(text='стержні до ручки стирачки', callback_data="стержні до ручки стирачки")
+    button3 = InlineKeyboardButton(text='стирачка для ручки стирачки', callback_data="стирачка для ручки стирачки")
 
     markup = InlineKeyboardMarkup()
-    markup.add(button1, button2)
+    markup.add(button1, button2, button3)
 
     bot.send_message(call.from_user.id, "виберіть товар:", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "ручка-стирачка")
 def test1(call):
-    bot.send_photo(call.message.chat.id, photo=open('images/pen1.jpg', 'rb'))
-    bot.send_message(call.message.chat.id, 'немає в наявності')
-
+    bot.send_photo(call.message.chat.id, photo=open('images/Дизайн без назви (27).png', 'rb'))
+    bot.send_message(call.message.chat.id, '''ручка-стирачка
+---------------------------------
+25 грн
+немає в наявності''')
 
 @bot.callback_query_handler(func=lambda call: call.data == "стержні до ручки стирачки")
 def test2(call):
-    bot.send_photo(call.message.chat.id, photo=open('images/15075389.jpg', 'rb'))
-    bot.send_message(call.message.chat.id, 'немає в наявності')
+    bot.send_photo(call.message.chat.id, photo=open('images/Дизайн без назви (29).png', 'rb'))
+    bot.send_message(call.message.chat.id, '''стержні до ручки стирачки
+---------------------------------
+5 грн
+немає в наявності''')
 
+@bot.callback_query_handler(func=lambda call: call.data == "стирачка для ручки стирачки")
+def test2(call):
+    bot.send_photo(call.message.chat.id, photo=open('images/Дизайн без назви (28).png', 'rb'))
+    bot.send_message(call.message.chat.id, '''стирачка для ручки стирачки
+---------------------------------
+10 грн
+немає в наявності''')
 
 bot.polling(none_stop=True)
